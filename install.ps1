ae966b72279641e5f6f29cf940907bdd11336607
Sam Hughes <samhughes1701@hotmail.co.uk>
2026-05-28 00:49:58 +0100

    Clean up comments in install.ps1
    Removed comments explaining the purpose of the script.

--- install.ps1
+++ install.ps1
@@ -1,9 +1,5 @@
 # Podratic new-starter bootstrap (Windows / PowerShell).
 #
-# This script is intentionally minimal. Its only job is to get the new starter
-# onto a known baseline (bun + gh, authenticated) and then hand off to the
-# Bun/TypeScript CLI in `podratics/onboard` which does the actual setup work.
-#
 # Usage:
 #   irm https://raw.githubusercontent.com/podratics/onboard-bootstrap/master/install.ps1 | iex
 #
